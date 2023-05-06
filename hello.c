ff1ca95e7defd2e7d67955b7dc9fdbe8e897f89a
ceres <ceres-r@LinuxLab>
2023-05-06 08:04:46 +0800

    lab1 hello

---
+++ hello.c
@@ -0,0 +1,18 @@
+#include<linux/module.h>
+#include<linux/kernel.h>
+#include<linux/init.h>
+static int init_hello(void)
+{
+	printk(KERN_INFO "Hello, World!\n");
+	return 0;
+}
+static void cleanup_hello(void)
+{
+	printk(KERN_INFO "Goodbye, world!\n");
+}
+module_init(init_hello);
+module_exit(cleanup_hello);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Ceres");
+MODULE_DESCRIPTION("A test project");
+
