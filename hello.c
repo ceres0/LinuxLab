79de650fc01c0159711839d95080dcba7a89290d
Ceres <ceres-r@fgf-server1>
2023-05-31 02:35:29 +0000

    lab1修改

--- hello.c
+++ hello.c
@@ -4,7 +4,18 @@
 #include <linux/sched/signal.h>
 static int init_hello(void)
 {
-	printk(KERN_INFO "Hello, World!\n");
+	printk(KERN_INFO "Hello, World!\n");    
+	printk(KERN_ALERT "pid=%i   command=%s ",current->pid,current->comm);
+    printk(KERN_ALERT "\n");
+
+    int pid_no=current->pid;
+    struct task_struct * task=current;
+    for_each_process(task) 
+   	{
+        if(task->pid==pid_no) break;
+           printk(KERN_ALERT "process id = %d tpid= %d",(int)task->pid,(int)task->comm);
+   	}
+	printk(KERN_ALERT "\n");
 	return 0;
 }
 static void cleanup_hello(void)
