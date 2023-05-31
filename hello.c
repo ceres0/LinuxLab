cdf7b0f2c8e5cbc1adfe5fbed6e1cf6c5d5312fe
Ceres <ceres-r@fgf-server1>
2023-05-31 02:13:53 +0000

    调整hello源码

--- hello.c
+++ hello.c
@@ -8,39 +8,40 @@
 #include <linux/kallsyms.h>
 #include <linux/kprobes.h>
 
-#define __NR_syscall 285	/* 系统调用号285 */
-unsigned long * sys_call_table;
+#define __NR_syscall 285 /* 系统调用号285 */
+unsigned long *sys_call_table;
 
 unsigned int clear_and_return_cr0(void);
 void setback_cr0(unsigned int val);
 static int sys_mycall(void);
 
-int orig_cr0;	/* 用来存储cr0寄存器原来的值 */
+int orig_cr0; /* 用来存储cr0寄存器原来的值 */
 unsigned long *sys_call_table = 0;
-static int (*anything_saved)(void);	/*定义一个函数指针，用来保存一个系统调用*/
+static int (*anything_saved)(void); /*定义一个函数指针，用来保存一个系统调用*/
 /*
  * 设置cr0寄存器的第17位为0
  */
-unsigned int clear_and_return_cr0(void)	
+unsigned int clear_and_return_cr0(void)
 {
-   	unsigned int cr0 = 0;
-   	unsigned int ret;
-    /* 前者用在32位系统。后者用在64位系统，本系统64位 */
-    //asm volatile ("movl %%cr0, %%eax" : "=a"(cr0));	
-   	asm volatile ("movq %%cr0, %%rax" : "=a"(cr0));	/* 将cr0寄存器的值移动到rax寄存器中，同时输出到cr0变量中 */
-    ret = cr0;
-	cr0 &= 0xfffeffff;	/* 将cr0变量值中的第17位清0，将修改后的值写入cr0寄存器 */
-	//asm volatile ("movl %%eax, %%cr0" :: "a"(cr0));
-	asm volatile ("movq %%rax, %%cr0" :: "a"(cr0));	/* 读取cr0的值到rax寄存器，再将rax寄存器的值放入cr0中 */
+	unsigned int cr0 = 0;
+	unsigned int ret;
+	/* 前者用在32位系统。后者用在64位系统，本系统64位 */
+	// asm volatile ("movl %%cr0, %%eax" : "=a"(cr0));
+	asm volatile("movq %%cr0, %%rax"
+				 : "=a"(cr0)); /* 将cr0寄存器的值移动到rax寄存器中，同时输出到cr0变量中 */
+	ret = cr0;
+	cr0 &= 0xfffeffff; /* 将cr0变量值中的第17位清0，将修改后的值写入cr0寄存器 */
+	// asm volatile ("movl %%eax, %%cr0" :: "a"(cr0));
+	asm volatile("movq %%rax, %%cr0" ::"a"(cr0)); /* 读取cr0的值到rax寄存器，再将rax寄存器的值放入cr0中 */
 	return ret;
 }
 
 /* 读取val的值到rax寄存器，再将rax寄存器的值放入cr0中 */
 void setback_cr0(unsigned int val)
-{	
+{
 
-	//asm volatile ("movl %%eax, %%cr0" :: "a"(val));
-	asm volatile ("movq %%rax, %%cr0" :: "a"(val));
+	// asm volatile ("movl %%eax, %%cr0" :: "a"(val));
+	asm volatile("movq %%rax, %%cr0" ::"a"(val));
 }
 
 /* 添加自己的系统调用函数 */
@@ -52,25 +53,26 @@ static int sys_mycall(void)
 }
 
 int noop_pre(struct kprobe *p, struct pt_regs *regs) { return 0; }
- 
-static struct kprobe kp = {   
-	.symbol_name = "kallsyms_lookup_name",  
+
+static struct kprobe kp = {
+	.symbol_name = "kallsyms_lookup_name",
 };
- 
+
 unsigned long (*kallsyms_lookup_name_fun)(const char *name) = NULL;
- 
+
 // 调用kprobe找到kallsyms_lookup_name的地址位置
 int find_kallsyms_lookup_name(void)
-{ 
+{
 	int ret = -1;
 	kp.pre_handler = noop_pre;
 	ret = register_kprobe(&kp);
-    if (ret < 0) {  
-	    printk(KERN_INFO "register_kprobe failed, error:%d\n", ret); 
-        return ret; 
+	if (ret < 0)
+	{
+		printk(KERN_INFO "register_kprobe failed, error:%d\n", ret);
+		return ret;
 	}
-	printk(KERN_INFO "kallsyms_lookup_name addr: %p\n", kp.addr); 
-	kallsyms_lookup_name_fun = (void*)kp.addr; 
+	printk(KERN_INFO "kallsyms_lookup_name addr: %p\n", kp.addr);
+	kallsyms_lookup_name_fun = (void *)kp.addr;
 	unregister_kprobe(&kp);
 	return ret;
 }
@@ -79,31 +81,31 @@ int find_kallsyms_lookup_name(void)
 static int __init init_addsyscall(void)
 {
 	printk("My syscall is starting。。。\n");
-    int ret;
-    ret = find_kallsyms_lookup_name();
-    if (ret < 0) {
-        printk(KERN_INFO "find kallsyms_lookup_name failed\n");
-        return ret;
-    }
-	sys_call_table = (unsigned long *)kallsyms_lookup_name_fun("sys_call_table");	/* 获取系统调用服务首地址 */
-   	printk("sys_call_table: 0x%p\n", sys_call_table);
-	anything_saved = (int(*)(void))(sys_call_table[__NR_syscall]);	/* 保存原始系统调用 */
-	orig_cr0 = clear_and_return_cr0();	/* 设置cr0可更改 */
-	sys_call_table[__NR_syscall] = (unsigned long)&sys_mycall;	/* 更改原始的系统调用服务地址 */
-	setback_cr0(orig_cr0);	/* 设置为原始的只读cr0 */
+	int ret;
+	ret = find_kallsyms_lookup_name();
+	if (ret < 0)
+	{
+		printk(KERN_INFO "find kallsyms_lookup_name failed\n");
+		return ret;
+	}
+	sys_call_table = (unsigned long *)kallsyms_lookup_name_fun("sys_call_table"); /* 获取系统调用服务首地址 */
+	printk("sys_call_table: 0x%p\n", sys_call_table);
+	anything_saved = (int (*)(void))(sys_call_table[__NR_syscall]); /* 保存原始系统调用 */
+	orig_cr0 = clear_and_return_cr0();								/* 设置cr0可更改 */
+	sys_call_table[__NR_syscall] = (unsigned long)&sys_mycall;		/* 更改原始的系统调用服务地址 */
+	setback_cr0(orig_cr0);											/* 设置为原始的只读cr0 */
 	return 0;
 }
 
 /*出口函数，卸载模块*/
 static void __exit exit_addsyscall(void)
 {
- 	orig_cr0 = clear_and_return_cr0();	/* 设置cr0中对sys_call_table的更改权限 */
-    	sys_call_table[__NR_syscall] = (unsigned long)anything_saved;	/* 设置cr0可更改 */
-    	setback_cr0(orig_cr0);	/* 恢复原有的中断向量表中的函数指针的值 */
-   	printk("My syscall exit....\n");	/* 恢复原有的cr0的值 */
+	orig_cr0 = clear_and_return_cr0();							  /* 设置cr0中对sys_call_table的更改权限 */
+	sys_call_table[__NR_syscall] = (unsigned long)anything_saved; /* 设置cr0可更改 */
+	setback_cr0(orig_cr0);										  /* 恢复原有的中断向量表中的函数指针的值 */
+	printk("My syscall exit....\n");							  /* 恢复原有的cr0的值 */
 }
 
 module_init(init_addsyscall);
 module_exit(exit_addsyscall);
-MODULE_LICENSE("GPL");
-
+// MODULE_LICENSE("GPL");
